a1f2831576b677ef024fb3fc66950d6ccbd3097b
timati19 <59282950+timati19@users.noreply.github.com>
2023-11-10 16:21:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,60 @@
 import sys
+
+import PIL.Image
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton
-from PIL import Image
+from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QFileDialog, QLabel, QVBoxLayout, QLineEdit, QTableView
 import torch
 from facenet_pytorch import MTCNN, InceptionResnetV1
+from PIL import Image
+import sqlite3
+from ai import FaceDetector
 
-class Face_control(QMainWindow):
+
+class Detector(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
-    def initUI(self):
-        self.setGeometry(600, 600, 600, 600)
-        self.image_but1 = QPushButton()
-        self.setWindowTitle('Отображение картинки')
-        self.image = QLabel(self)
+    @classmethod
+    def b(self):
+        self.ai = FaceDetector
 
-    def Face_control(self):
-        self.im.putalpha(self.alpha.value())
-        self.im.save('new.png')
-        self.pixmap = QPixmap('new.png')
-        self.image.setPixmap(self.pixmap)
+    def initUI(self):
+        self.setGeometry(0, 0, 600, 600)
+        self.setWindowTitle('Детектор номеров')
+
+        self.btn = QPushButton('Выбрать фото', self)
+        self.btn.move(20, 500)
+        self.btn.clicked.connect(self.load_image)
+        self.label1 = QLabel('Изображение:', self)
+        self.label1.move(10, 30)
+        self.label1.hide()
+        self.layout1 = QVBoxLayout()
+        self.layout1.addStretch()
+        self.open_btn = QPushButton('Выбрать БД', self)
+        self.open_btn.move(320, 500)
+        #self.open_btn.clicked.connect(self.openWin)
+
+    def load_image(self):
+        file_name = QFileDialog.getOpenFileName()
+        if file_name:
+            self.label3 = QLabel(self)
+            self.img = QPixmap(file_name)
+            self.label3.setPixmap(self.img)
+            self.label3.show()
+
+
+
+
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Face_control()
+    ex = Detector()
     ex.show()
     sys.exit(app.exec())
 
-
 mtcnn = MTCNN()
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
-
-
-def compare(known_face, unknown_face):
-    known_image = Image.open(known_face)
-    unknown_image = Image.open(unknown_face)
-    known_face = mtcnn(known_image)
-    unknown_face = mtcnn(unknown_image)
-    known_embedding = resnet(known_face.unsqueeze(0))
-    unknown_embedding = resnet(unknown_face.unsqueeze(0))
-    distance = torch.nn.functional.pairwise_distance(known_embedding, unknown_embedding)
-    similarity_percentage = (1 - distance.item()) * 100
-    if similarity_percentage >= 40:
-        print(f'Это один и тот же человек! {round(similarity_percentage, 1)} %!')
-    else:
-        print(f'😭{round(similarity_percentage, 1)} %(((')
-
-compare('known_face.jpg', 'unknown_face.jpg')
