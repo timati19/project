bdd14c9d8214c8bac2d3f1ac80a6385533743689
timati19 <59282950+timati19@users.noreply.github.com>
2023-11-11 03:33:24 +0700

    Update ai.py

--- ai.py
+++ ai.py
@@ -1,23 +1,14 @@
-import torch
-from facenet_pytorch import MTCNN, InceptionResnetV1
-import sqlite3
-from PIL import Image
-from roboflow import Roboflow
-import cv2
-import numpy as np
-import face_recognition
-
-
 class FaceDetector:
-    def __init__(self, db):
-        # con = sqlite3.connect(db)
-        # mtcnn = MTCNN()
-        # resnet = InceptionResnetV1(pretrained='vggface2').eval()
+    def __init__(self):
+        from roboflow import Roboflow
+        from facenet_pytorch import MTCNN, InceptionResnetV1
         rf = Roboflow(api_key="nwjBYhl5xVcVEpDaG187")
         project = rf.workspace().project("face-detection-vswnd")
         self.model = project.version(11).model
+        self.mtcnn = MTCNN()
+        self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
 
-    def crop_face(self, face,):
+    def crop_face(self, face):
         b = self.model.predict(face, confidence=40, overlap=30).json()
         for prediction in b['predictions']:
             x1 = prediction['x'] - prediction['width'] // 2
@@ -27,13 +18,17 @@ class FaceDetector:
             coords = (round(x1), round(y1), round(x2), round(y2))
         return coords
 
-    def compare(self, face1, face2, coords1, coords2):
-        self.face1 = Image.open(face1)
-        self.face2 = Image.open(face2)
-        # Получение кодировок лиц
-        encoding1 = face_recognition.face_encodings(self.face1)[0]
-        encoding2 = face_recognition.face_encodings(self.face2)[0]
+    def compare(self, face1, face2, coords):
+        from PIL import Image
+        import torch
+        self.face1 = face1
+        self.face2 = Image.open(face2).crop(coords)
+        known_face = self.mtcnn(self.face1)
+        unknown_face = self.mtcnn(self.face2)
+        known_embedding = self.resnet(known_face.unsqueeze(0))
+        unknown_embedding = self.resnet(unknown_face.unsqueeze(0))
         # Вычисление процентной схожести
-        similarity = face_recognition.face_distance([encoding1], encoding2)[0] * 100
-        return similarity
+        distance = torch.nn.functional.pairwise_distance(known_embedding, unknown_embedding)
+        similarity = (1 - distance.item()) * 100
 
+        return similarity
