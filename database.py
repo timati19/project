469e61a98404c158af4c89be5dc433ff18c22b0b
timati19 <59282950+timati19@users.noreply.github.com>
2023-11-11 03:33:12 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,23 +1,40 @@
-import datetime
-import sqlite3
-import PIL
-
-
 class FaceTable:
     def __init__(self, db):
+        import sqlite3
         self.db = db
+        self.con = sqlite3.connect(self.db)
+        self.cursor = self.con.cursor()
 
     def get_faces(self):
-        con = sqlite3.connect(self.db)
-        cursor = con.cursor()
-        cursor.execute(f'''
-                            SELECT human, face FROM humans
+        self.cursor.execute(f'''
+                            SELECT id, face, human face FROM humans
                         ''')
-        rows = cursor.fetchall()
+        rows = self.cursor.fetchall()
         return rows
 
-    def set_dates(self, ):
+    def set_date(self, id):
+        import datetime as dt
+        query = self.cursor.execute(f'''
+                                    select in_date, out_date from humans where id = {id}
+                                ''').fetchall()
+        print(query)
+        if not query[0][0]:
+            self.cursor.execute(f'''
+                                update humans set in_date = '{dt.datetime.now().strftime("%H:%M")}' 
+                                where id = '{id}'
+                                ''').fetchall()
+            self.con.commit()
+            return
 
-
-face = FaceTable('faces.db')
-print(face.get_faces())
+        if not query[0][1]:
+            self.cursor.execute(f'''
+                                update humans set out_date = '{dt.datetime.now().strftime("%H:%M")}' 
+                                where id = '{id}'
+                                ''').fetchall()
+            self.con.commit()
+            return
+        self.cursor.execute(f'''
+                                        update humans set in_date = '{dt.datetime.now().strftime("%H:%M")}', out_date = ''
+                                        where id = '{id}'
+                                        ''').fetchall()
+        self.con.commit()
